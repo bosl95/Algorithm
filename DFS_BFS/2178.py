4e7bfb6cca7a32eb268d867134d06059a26e7321
sori <bosl95@naver.com>
2020-03-26 19:32:10 +0900

    DFS BFS 2178

--- DFS_BFS/2178.py
+++ DFS_BFS/2178.py
@@ -21,21 +21,25 @@
 방문하는 곳이 몇번째 방문인지 count ( 13번 참조 )
 '''
 
-import sys; input = sys.stdin.readline
+# import sys; input = sys.stdin.readline
 n, m = map(int, input().split())
-maze = [ list(map(int, input().strip())) for _ in range(n) ]
-visit = [ [0]*m for _ in range(n) ]; move = [ [1, 0], [-1, 0], [0, 1], [0, -1] ]
+maze = [ list(map(int, input())) for _ in range(n) ]
+visit = [ [0]*m for _ in range(n) ]
 queue = [[0, 0]]; visit[0][0] = 1
 while queue:
-    node = queue.pop(0)
-    visit.append(node)
+    x, y = queue.pop(0)
+    visit.append([x, y])
 
-    for mm in move:
-        new = [node[0] + mm[0], node[1] + mm[1]]
-        if new[0] >= 0 and new[0] < n and new[1] >= 0 and new[1] < m:
-            if maze[new[0]][new[1]]==1 and new not in visit and new not in queue:
-                queue.append(new)
-                visit[new[0]][new[1]] = visit[node[0]][node[1]] + 1
+    for mx, my in [1, 0], [-1, 0], [0, 1], [0, -1]:
+        xx, yy = x + mx, y + my
+        if 0 <=  xx < n and 0 <= yy < m and maze[xx][yy]==1 and visit[xx][yy] == 0:
+            queue.append([xx, yy])
+            visit[xx][yy] = visit[x][y] + 1
 print(visit[n-1][m-1])
 
 '''     BFS로 해야되는 문제    '''
+'''
+35줄에 visit[xx][yy]===0 이랑
+xx, yy not in visit  이랑
+시간차가 큼
+'''
