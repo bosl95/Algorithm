2a9aac6b5f490490381377e88dbf87890714f0f2
sor <bosl95@naver.com>
2020-05-03 17:58:50 +0900

    분할정복 - 6549 # 메모리 초과 이후 스택으로 풀어보았으나 스택이 어떻게 분할정복을 이용하는지 이해했음에 불구하고 반례라고 생각되는 예시들이 통과되는걸 이해할 수 없었다.

--- 23]분할정복/[6549]히스토그램에서 가장 큰 직사각형/[6549]히스토그램에서 가장 큰 직사각형.py
+++ 23]분할정복/[6549]히스토그램에서 가장 큰 직사각형/[6549]히스토그램에서 가장 큰 직사각형.py
@@ -1,20 +1,15 @@
-import sys;input = sys.stdin.readline
-h_arr = []
 while True:
-    x = input()
-    if x[0]=='0':
-        break
-    h_arr.append(list(map(int, x.split()))) # 입력
-
-def DAC(arr):
-    l = len(arr)
-    if l == 1: return arr[0]
-    else:
-        m = min(arr)
-        idx = arr.index(m)
-        if idx==0:
-            idx += 1
-        return max(DAC(arr[:idx]), DAC(arr[idx:]), l*m)
-
-for h in h_arr:
-    print(DAC(h))
+    N, *H=list(map(int, input().split()))
+    if N == 0: break
+    H.append(0)
+    s=[]
+    ans=0
+    for i, h in enumerate(H):
+        while s and H[s[-1]] > h:  # h보다 작은 값이 나올때까지 실행
+            th = H[s.pop()]
+            w = i - s[-1] - 1 if s else i
+            # 스택이 비어있다 : 최소 높이의 인덱스라는 의미이므로 가로는 i
+            # 아닌 경우는 i에서 s의 top인 인덱스를 빼준다.
+            if ans < th * w: ans = th * w
+        s.append(i)
+    print(ans)
