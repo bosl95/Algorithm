670782a7e20247008bc6268b52e5ce189859dc07
sor <bosl95@naver.com>
2020-09-11 00:11:08 +0900

    2020-09-08 [Algorithm] Prim_MST.py Add

--- MST/Prim_MST.py
+++ MST/Prim_MST.py
@@ -4,24 +4,26 @@ from heapq import *
 def prim(start_node, edges):
     mst = list()
 
+    # 인접한 정점을 갖는 dict 형태
     adjacent_edges = defaultdict(list)
     for w, u, v in edges:
         adjacent_edges[u].append((w, u, v))
         adjacent_edges[v].append((w, v, u))
 
-    connected_nodes = set(start_node)
-    candidate_edge_list = adjacent_edges[start_node]
-    heapify(candidate_edge_list)
 
-    while candidate_edge_list:
-        w, u, v = heappop(candidate_edge_list)
-        if v not in connected_nodes:
-            connected_nodes.add(v)
-            mst.append((w, u, v))
+    connected_nodes = set(start_node)                   # 연결된 노드 = 처음 노드로 초기화
+    candidate_edge_list = adjacent_edges[start_node]    # 첫 노드에 인접한 노드를 후보 노드 리스트에 삽입
+    heapify(candidate_edge_list)                        # 우선 순위 큐로 변환
 
-            for edge in adjacent_edges[v]:
-                if edge[2] not in connected_nodes:
-                    heappush(candidate_edge_list, edge)
+    while candidate_edge_list:                          # 후보 노드 리스트 방문
+        w, u, v = heappop(candidate_edge_list)          # 가장 가중치가 낮은 순으로 꺼내온다.
+        if v not in connected_nodes:                    # 후보 노드가 아직 연결되지 않았다면
+            connected_nodes.add(v)                      # 연결된 노드 리스트에 삽입
+            mst.append((w, u, v))                       # mst 배열에 삽입
+
+            for edge in adjacent_edges[v]:              # 선택된 후보 노드에서 이어 방문할 노드가
+                if edge[2] not in connected_nodes:      # 연결된 노드가 아니라면
+                    heappush(candidate_edge_list, edge) # 후보 노드 리스트에 삽입
 
     return mst
 
