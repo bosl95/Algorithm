a6e6f2c83fcac95f50d8df6f70874447571dc563
sor <bosl95@naver.com>
2020-10-15 04:41:43 +0900

    2020-10-14 [String] [1786]찾기 Add

--- STRING/[1786]찾기/[1786]찾기.py
+++ STRING/[1786]찾기/[1786]찾기.py
@@ -1,21 +1,35 @@
-import sys
-input=sys.stdin.readline
+def makeF(p):
+    n = len(p)
+    F = [0] * len(p)
+    j = 0
+    for i in range(1, n):
+        if j>0 and p[i] != p[j]:    # p[i]와 p[j]가 같지 않으면 j = F[j-1]
+            j = F[j-1]
+        if p[i] == p[j]:
+            F[i] = j+1              # p[i] == p[j]면 F[i] = j+1(j는 인덱스이므로)
+            j += 1                  # j 한칸 이동
+    return F
 
-def solution(t, p):
-    m, n = len(t), len(p)
-    s, stack = 0, []
+def kmp(s, p):
+    ans = []
+    fail = makeF(p)
+    n, m, j = len(s), len(p), 0
 
-    for i in range(m):
-        if t[i] == p[0]:
-            if t[i:i+n] == p:
-                s += 1
-                stack.append(i+1)
+    for i in range(n):
+        if j>0 and s[i]!=p[j]:
+            j = fail[j-1]
+        if s[i] == p[j]:
+            if j == m-1:
+                ans.append(i-m+2)
+                j = fail[j]
+            else:
+                j+=1
 
-    print(s)
-    for i in range(s):
-        print(stack[i], end=' ')
+    print(len(ans))
+    for a in ans:
+        print(a, end=' ')
 
 
-T = input()
-P = input()
-solution(T[:-1], P[:-1])
+t = input()
+p = input()
+kmp(t, p)
