af11552409cc9b7c7e907b36ee243bdd8a96ac9e
sori <bosl95@naver.com>
2020-10-18 15:56:31 +0900

    20-10-17 [algoritm] readme.md update

--- SIMULATION/[18808]스티커 붙이기/[18808]스티커 붙이기.py
+++ SIMULATION/[18808]스티커 붙이기/[18808]스티커 붙이기.py
@@ -1,8 +1,52 @@
 import sys
+from copy import deepcopy
+
 input=sys.stdin.readline
 
+def check(x, y, v, nm, sticker):
+    v = deepcopy(v)
+    for i in range(nm[0]):
+        for j in range(nm[1]):
+            if sticker[i][j] == 1:
+                if v[x+i][y+j]==1:
+                    return (False, None)
+                elif v[x+i][y+j]==0:
+                    v[x+i][y+j] = 1
+    return (True, v)
+
+def DFS(i, x, note, cnt):
+    if i == K :
+        print(cnt)
+        sys.exit()
+    if x == 0:
+        ch = False
+        for a in range(N-st_nm[i][0]):
+            for b in range(M-st_nm[i][1]):
+                (ch, _v) = check(a, b, note, st_nm[i], sticker[i][x])
+                if ch: break
+        if ch:
+            for k in range(4):
+                DFS(i+1, k, _v, num[i]+cnt)
+        else:
+            for k in range(4):
+                DFS(i+1, k, note, cnt)
+    elif x == 1:
+        ch = False
+        for a in range(M-st_nm[i][1]):
+            for b in range(N-st_nm[i][0]):
+                (ch, _v) = check(a, b, note, st_nm[i], sticker[i][x])
+                if ch: break
+        if ch:
+            for k in range(4):
+                DFS(i + 1, k, _v, num[i] + cnt)
+        else:
+            for k in range(4):
+                DFS(i + 1, k, note, cnt)
+
+
+
 N, M, K = map(int, input().split())
-sticker = []; st_nm = []; num = []  # 스티커 배열, 스티커 n1, m1, 스티커 "1"의 개수
+sticker = []; st_nm = []; num = []  # 스티커 배열(0도/90도/180도/270), 스티커 n1, m1, 스티커 "1"의 개수
 for _ in range(K):
     x, y = map(int, input().split())
     st_nm.append((x, y))
@@ -13,4 +57,5 @@ for _ in range(K):
         tmp.append(z)
     num.append(count)
 
-DFS(0, 0, )
+notebook = [[0]*M for _ in range(N)]
+DFS(0, 0, notebook, 0)
