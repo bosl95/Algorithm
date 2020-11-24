1ba7475a1722e440ba4bdc1a22ba764a658c6bd0
sor <bosl95@naver.com>
2020-11-24 22:14:47 +0900

    2020-11-24 [dp] [11727]2xn타일링2 Add

---
+++ DYNAMIC PROGRAMMING/[11727]2xn타일링2/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	
+	
+	public static void main(String[] args) throws NumberFormatException, IOException  {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[n+1];
+		
+		for (int i=0;i<n+1;i++) {
+			if (i==0) dp[i] = 0;
+			else if (i==1) dp[i]=1;
+			else if (i==2) dp[i]=3;
+			else dp[i] = (dp[i-1]+dp[i-2]*2)%10007;
+		}
+		System.out.println(dp[n]);
+	}
+
+}
