95f48b5b7a8305ef4f68fecb27c0da6b94ca9aaf
sori <bosl95@naver.com>
2020-12-05 12:19:54 +0900

    feat(queue) : add backjoon_2164

---
+++ QUEUE_DEQUE/Backjoon_2164/backjoon_2164.java
@@ -0,0 +1,25 @@
+package QUEUE_DEQUE.Backjoon_2164;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class backjoon_2164 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<>();
+
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size()!=1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
