77277e348d8ed0ff3f19d8209b0ad7a2f58d9870
sor <bosl95@naver.com>
2020-12-23 17:48:52 +0900

    feat : add backjoon_10807

--- ARRAY/Backjoon_10807/Main.java
+++ ARRAY/Backjoon_10807/Main.java
@@ -7,10 +7,9 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.valueOf(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         String[] str = br.readLine().split(" ");
-        int v = Integer.valueOf(br.readLine());
+        int v = Integer.parseInt(br.readLine());
 
         int result = 0;
         for (int i=0; i<N; i++) {
