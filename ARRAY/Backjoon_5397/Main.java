c4b4217ac69741682051c35011c2980df5db23a1
sor <bosl95@naver.com>
2020-12-23 18:20:23 +0900

    feat : add backjoon_5397

--- ARRAY/Backjoon_5397/Main.java
+++ ARRAY/Backjoon_5397/Main.java
@@ -1,38 +1,47 @@
 package ARRAY.Backjoon_5397;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        LinkedList<String> key_logger = new LinkedList<>();
-
         int num = Integer.parseInt(br.readLine());
-        int cursor;
 
-        while (num-- > 0) {
-            key_logger.clear();
-            cursor = 0;
-            for (String word : br.readLine().split("")) {
-                if (word.equals("<")) {
-                    cursor = !key_logger.isEmpty() ? --cursor : cursor;
-                } else if (word.equals(">")) {
-                    cursor = cursor < key_logger.size() ? ++cursor : cursor;
-                } else if (word.equals("-")) {
-                    if (cursor > 0) {
-                        key_logger.remove(--cursor);
-                    }
-                } else {
-                    key_logger.add(cursor++, word);
-                }
-            }
-            sb.append(String.join("", key_logger));
-            sb.append("\n");
+        for (int i = 0; i < num; i++) {
+            String pwd = br.readLine();
+            System.out.println(getPwd(pwd));
         }
-        System.out.print(sb.toString());
+    }
+
+    private static String getPwd(String pwd) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> pre = new Stack<>();
+        Stack<Character> post = new Stack<>();
+
+        for (int i = 0; i < pwd.length(); i++) {
+            switch (pwd.charAt(i)) {
+                case '<':
+                    if (!pre.isEmpty()) post.push(pre.pop());
+                    break;
+                case '>':
+                    if (!post.isEmpty()) pre.push(post.pop());
+                    break;
+                case '-':
+                    if (!pre.isEmpty()) pre.pop();
+                    break;
+                default:
+                    pre.push(pwd.charAt(i));
+                    break;
+            }
+        }
+        while (!post.isEmpty())  {
+            pre.push(post.pop());
+        }
+        for (int i=0; i<pre.size(); i++) {
+            sb.append(pre.elementAt(i));
+        }
+
+        return sb.toString();
     }
 }
