a3a7f2bd8f1a53804390c37dfa446717eef75b84
sor <bosl95@naver.com>
2021-12-05 19:34:39 +0900

    feat: programmers 최고의 등굣길 2번째 버전

--- Programmers/등굣길/Main2.java
+++ Programmers/등굣길/Main2.java
@@ -1,19 +1,19 @@
-package Programmers.TEST;
+package Programmers.등굣길;
 
-import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 
 public class Main2 {
     public static void main(String[] args) {
-        final Solution2 solution2 = new Solution2();
+        final Solution solution2 = new Solution();
         final int result = solution2.solution(4, 3, new int[][]{{2, 2}});
         System.out.println(result);
     }
 }
 
-class Solution2 {
+class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] area = new int[n][m];
+        int[][] area = new int[m][n];
 
         for (int i = 0; i < puddles.length; i++) {
             int x = puddles[i][0] - 1;
@@ -21,7 +21,7 @@ class Solution2 {
             area[x][y] = -1;
         }
 
-        Deque<int[]> deque = new ArrayDeque<>();
+        Deque<int[]> deque = new LinkedList<>();
         deque.add(new int[]{0, 0});
         area[0][0] = 1;
 
@@ -32,23 +32,22 @@ class Solution2 {
             if (area[x][y] > 1) continue;
 
             if (x > 0 && area[x - 1][y] != -1) {
-                area[x][y] += area[x - 1][y] % 1000000007;
+                area[x][y] += area[x - 1][y] % 1_000_000_007;
             }
 
             if (y > 0 && area[x][y - 1] != -1) {
-                area[x][y] += area[x][y - 1] % 1000000007;
+                area[x][y] += area[x][y - 1] % 1_000_000_007;
             }
 
-
-            if (x + 1 < n && area[x + 1][y] == 0) {
+            if (x + 1 < m&& area[x + 1][y] == 0) {
                 deque.addLast(new int[]{x + 1, y});
             }
 
-            if (y + 1 < m && area[x][y + 1] == 0) {
+            if (y + 1 < n && area[x][y + 1] == 0) {
                 deque.addLast(new int[]{x, y + 1});
             }
         }
 
-        return area[n - 1][m - 1] % 1000000007;
+        return area[m - 1][n - 1] % 1000000007;
     }
 }
